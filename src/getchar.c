34283a428b4cca520eecfa7ec537b91a604add4f
Piotr <79946580+motocyklista@users.noreply.github.com>
2025-11-09 22:16:24 +0100

    Create getchar.c

---
+++ src/getchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char tekst[200];
+    int i = 0;
+    int znak;  // int, żeby rozróżnić EOF
+    int male = 0, duze = 0, cyfry = 0, inne = 0;
+
+    printf("Wpisz tekst (zakoncz enterem)\n");
+
+    while ((znak = getchar()) != '\n' && znak != EOF && i < sizeof(tekst) - 1) {
+        tekst[i++] = (char)znak;
+
+        if (znak >= 'A' && znak <= 'Z') duze++;
+        else if (znak >= 'a' && znak <= 'z') male++;
+        else if (znak >= '0' && znak <= '9') cyfry++;
+        else inne++;
+    }
+
+    tekst[i] = '\0';
+
+  printf("Wynik: %s, DUZE: %d, MALE: %d, CYFRY: %d, INNE: %d\n", tekst, duze, male, cyfry, inne);
+    if (znak == EOF) {
+        printf("\nKoniec pliku (EOF) został osiągnięty.\n");
+    }
+
+    return 0;
+}
